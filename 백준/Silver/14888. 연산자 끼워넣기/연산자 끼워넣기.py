014d2a894e9201b0e8ba0af1e13e8101533ba34a
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-08-25 15:21:41 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 92 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -7,7 +7,7 @@ op = list(map(int, read().split())) #순서대로 +, -, x, //
 
 maxi = -1e10 #젤 작은 수로 초기화 해두고 이거보다 큰거 나오면 새로 업데이트
 mini = 1e10 #젤 큰 수로 초기화 해두고 이거보다 작은거 나오면 새로 업데이트
-def dfs(depth, result, op):
+def dfs(depth, result):
   global mini, maxi
   if depth == n: #마지막 수까지 계산이 끝났다면?
     mini = min(mini, result)#minimum 업데이트
@@ -15,21 +15,21 @@ def dfs(depth, result, op):
   else: #아직 마지막 수가 아니라면
     if op[0] > 0: #+를 아직 쓸 수 있다면
       op[0] -= 1 #+를 한번 쓰고
-      dfs(depth+1, result + arr[depth], op) #dfs한 다음
+      dfs(depth+1, result + arr[depth]) #dfs한 다음
       op[0] += 1
     if op[1] > 0:
       op[1] -= 1
-      dfs(depth+1, result - arr[depth], op)
+      dfs(depth+1, result - arr[depth])
       op[1] += 1
     if op[2] > 0:
       op[2] -= 1
-      dfs(depth+1, result * arr[depth], op)
+      dfs(depth+1, result * arr[depth])
       op[2] += 1
     if op[3] > 0:
       op[3] -= 1
-      dfs(depth+1, int(result / arr[depth]), op)
+      dfs(depth+1, int(result / arr[depth]))
       op[3] += 1
 
-dfs(1,arr[0], op)
-print(int(maxi))
-print(int(mini))
+dfs(1,arr[0])
+print(maxi)
+print(mini)
