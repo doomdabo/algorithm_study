9997bbccc648eb855908707169e77d837152eaf7
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-08-31 21:51:27 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,22 +1,20 @@
 def solution(N, stages):
     answer = []
-    arr = []
-    k = len(stages) #전체 사람의 수
-
-    for i in range(1,N+1): #N개의 스테이지에 대해 조사
+    for i in range(1,N+1):
+        mo = 0 #분모 - 스테이지에 도달한 플레이어 수
+        ja = 0 #분자 - 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수
+        for stage in stages:
+            if stage>=i: #스테이지에 도달한 플레이어 수
+                mo += 1
+                if stage == i :
+                    ja += 1
         fail = 0
-        for j in range(len(stages)): 
-            if i == stages[j]:
-                fail+=1
-        if fail != 0:
-            fail_percent = fail/k
-            arr.append((i,fail_percent))
-        else:
-            arr.append((i,0))
-        k-= fail #실패 유저들을 빼서, 스테이지에 도달한 플레이어 수를 업데이트 해줌
-            
-    arr=sorted(arr,key = lambda x:-x[1])
-    for i in arr:
-        answer.append(i[0])
-    return answer
-
+        if mo != 0:
+            fail = ja/mo
+        answer.append((fail,i))
+    answer = sorted(answer,key = lambda x:(-x[0],x[1]))
+    arr = []
+    for i in answer:
+        arr.append(i[1])
+    return arr
+        
