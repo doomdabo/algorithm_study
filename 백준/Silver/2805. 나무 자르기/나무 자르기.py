e01d21f85c4e8f4de9b1f0dae1e30091f01d3a48
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-09-09 01:31:18 +0900

    [Silver II] Title: 나무 자르기, Time: 4512 ms, Memory: 143400 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -7,14 +7,14 @@ tree = list(map(int,read().split()))
 tree.sort()
 st,end = 1,tree[-1]
 result = 0
-while st<end:
+while st<=end:
   mid = (st+end)//2
   sum = 0
-  for i in tree:
+  for i in tree: # for i in range(len(tree)): 이렇게 하면 python통과 못함..
     if i > mid:
       sum += i - mid
   if sum < m:
-    end = mid
+    end = mid -1
   else:
     result = mid
     st = mid + 1
