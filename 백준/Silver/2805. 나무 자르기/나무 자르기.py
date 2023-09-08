0af891603932651e6708d7211ac371aab1bf5ac0
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-09-09 01:20:08 +0900

    [Silver II] Title: 나무 자르기, Time: 532 ms, Memory: 259740 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,21 @@
+import sys
+read = sys.stdin.readline
+
+n,m = map(int,read().split())
+tree = list(map(int,read().split()))
+
+tree.sort()
+st,end = 1,tree[-1]
+result = 0
+while st<end:
+  mid = (st+end)//2
+  sum = 0
+  for i in range(len(tree)):
+    if tree[i] > mid:
+      sum += tree[i] - mid
+  if sum < m:
+    end = mid
+  else:
+    result = mid
+    st = mid + 1
+print(result)
