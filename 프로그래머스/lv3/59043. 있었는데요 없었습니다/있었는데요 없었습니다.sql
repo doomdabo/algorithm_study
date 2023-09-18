4eb7c5fb27ed7b0fe7e20074ea7785d03ac98984
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-09-18 18:48:19 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,4 @@
--- 코드를 입력하세요
-SELECT i.ANIMAL_ID, i.NAME
-from animal_ins i join animal_outs o on i.animal_id= o.animal_id
-where i.datetime>o.datetime
-order by i.datetime
+SELECT I.ANIMAL_ID, I.NAME
+FROM ANIMAL_INS I JOIN ANIMAL_OUTS O ON I.ANIMAL_ID = O.ANIMAL_ID
+WHERE I.DATETIME > O.DATETIME
+ORDER BY I.DATETIME
