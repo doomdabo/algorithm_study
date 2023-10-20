591dbd034153861c488e940c00457f62231a1fdc
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2023-10-20 21:20:38 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 12496 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,29 +1,33 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-int a[1000000];
-int ans[1000000];
-int main(void)
-{
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  int n;
-  stack<int> s;
-  cin >> n;
-  for (int i = 0; i < n; i++)
-    cin >> a[i];
-  for (int i = n - 1; i >= 0; i--)
-  {
-    while (!s.empty() && s.top() <= a[i])
-      s.pop();
-    //만약 스택 비어있지 않고 들어온 수가 top보다 작으면 top에 있는거 pop
-    if (s.empty())
-      ans[i] = -1; //더 큰수가 없다? 그럼 -1
-    else
-      ans[i] = s.top(); //정답 저장!
-    s.push(a[i]);
-  }
-  for (int i = 0; i < n; i++)
-  {
-    cout << ans[i] << " ";
-  }
+int n;
+int arr[1000001];
+int ans[1000001];
+stack<int> s;
+int main(void) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	for (int i = n - 1; i >= 0; i--) {
+		while (!s.empty() && s.top() <= arr[i]) {
+			//stack에 뭐가 있는데 원하는게 아니면 계속 pop
+			s.pop();
+		}
+		if (s.empty()) {
+			//원하는게 결국 없었어?
+			ans[i] = -1; //답은 -1
+		}
+		else {
+			//원하는게 나왔어?
+			ans[i] = s.top();
+		}
+		s.push(arr[i]);
+	}
+	for (int i = 0; i < n; i++) {
+		cout << ans[i] << " ";
+	}
 }
