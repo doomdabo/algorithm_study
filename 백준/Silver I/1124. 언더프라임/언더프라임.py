05bc724a3c540d1e0627078006495aca0b8fd25c
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2024-02-14 17:29:37 +0900

    [Silver I] Title: 언더프라임, Time: 7556 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver I/1124. 언더프라임/언더프라임.py
+++ 백준/Silver I/1124. 언더프라임/언더프라임.py
@@ -10,25 +10,31 @@ prime=[i for i in range(2,b+1) if sieve[i]==True]
 #print(prime)
 ans=[]
 
-for i in range(a,b+1):
-  cnt=0
-  k = i
-  while True:
-    if k in prime:
-      cnt+=1
+# for i in range(a,b+1):
+#   cnt=0
+#   k = i
+#   while True:
+#     if k in prime:
+#       cnt+=1
+#       break
+#     for j in prime:
+#       if k%j==0: #나눠 떨어지면
+#         k = k//j
+#         cnt+=1
+#         break
+#   #print(i,cnt)
+#   ans.append(cnt)
+dp=[0]*(b+1)
+for i in range(2,b+1):
+  for j in prime:
+    if i%j==0:
+      dp[i]=dp[i//j]+1
       break
-    for j in prime:
-      if k%j==0: #나눠 떨어지면
-        k = k//j
-        cnt+=1
-        break
-  #print(i,cnt)
-  ans.append(cnt)
-
+#print(dp)
 #print(ans)
 realans=0
-for i in ans:
-  if i in prime:
+for i in range(a,b+1):
+  if dp[i] in prime:
     realans+=1
 print(realans)
   
