1cd88194cf8660195ac506257050f90405f6f292
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2024-09-26 00:28:47 +0900

    [Gold IV] Title: 최단경로, Time: 580 ms, Memory: 68380 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,38 +1,30 @@
-import heapq
 import sys
-read = sys.stdin.readline
+import heapq
+input = sys.stdin.readline
+
+v,e = map(int,input().split())
+start = int(input()) #시작정점의 번호
+graph = [[] for _ in range(v+1)] #간선을 기준으로 저장
 INF = int(1e9)
-v, e = map(int,read().split())
-graph = [[] for _ in range(v+1)] #연결이 어디어디 되어있나 확인
-k = int(read())
-#입력받기
+dist = [INF]*(v+1) #최단거리 테이블
+
 for _ in range(e):
-    a,b,c = map(int,read().split())
-    graph[a].append((b,c))
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c)) #u번정점은 v와 연결되어있고 거리 w
 
-dist = [INF] * (v+1)
-
-def dijkstra(start):
-    dist[start] = 0 #시작점은 0으로 초기화
-    q = []
-    heapq.heappush(q,(0,start)) #처음 거리: 0, 시작위치: start
-    while q:
-        #큐 비어있지 않으면 가장 짧은거 불러오기
-        cost, cur = heapq.heappop(q)
-        #이게 전에 처리 된건지(이미 더 짧은게 있나 확인)
-        if dist[cur] < cost: #큐에서 방금 나온게 더 큰거면 그거 무시하는것
-            continue
-        for i in graph[cur]:
-            #현재위치에서 연결된 모든 점을 살필거다
-            #일단 거리 계산을 하자
-            newcost = cost + i[1]
-            #만약 지금까지 온 거리보다 짧은게 나왔으면
-            if newcost<dist[i[0]]:
-                dist[i[0]] = newcost #새로 구한 값으로 갱신해줌
-                heapq.heappush(q,(newcost,i[0]))
-
-
-dijkstra(k)
+hq = []
+dist[start] = 0 #시작정점의 거리 0으로 초기화
+heapq.heappush(hq,(0,start)) #(거리, 정점번호)로 값 계속 넣음
+while hq: #큐에 원소 있으면 계속 진행
+    cur_cost, cur_v = heapq.heappop(hq)
+    if(dist[cur_v] < cur_cost): #만약 옛날에 저장된애임. 이미 더 싼 거리가 나와서 갱신 했는데 갱신되기 이전애라면 걍 볼필요 없이 팝만하면됨
+        continue
+    for nxt_v, nxt_cost in graph[cur_v]: #현재정점과 이어진 정점을 확인하자
+        if dist[nxt_v] > nxt_cost + cur_cost: #다음 연결된 애의 거리를 확인하자.
+            #다음 연결된 애 저장되어 있는 거리가 무언가 거쳐가는거보다 크면, 더 작은거로 갱신하자
+            dist[nxt_v] = nxt_cost + dist[cur_v]
+            heapq.heappush(hq,(dist[nxt_v],nxt_v))
+#rint(dist)
 for i in range(1,v+1):
     if dist[i] == INF:
         print("INF")
