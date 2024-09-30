28d67ad039a52a53bb9e069267e3a99ee55e9beb
Bokyung Kwon <72394295+doomdabo@users.noreply.github.com>
2024-09-30 21:55:36 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,25 +1,25 @@
-import sys
+n,m = map(int,input().split())
 
-read = sys.stdin.readline
-n,m = list(map(int,read().split()))
+#1부터 n까지 자연수 중에서 중복 없이 m개를 고른 수열
 
-#1~n까지 자연수 중 중복없이 m개를 고르는 수열
+arr = [i+1 for i in range(n)]
+temp = []
+vis = [False] * (n+1)
+cnt = 0
+def func(cnt):
+    if cnt == m:
+        for i in temp:
+            print(i,end=' ')
+        print()
 
-#1~n까지 자연수 담은 배열 만들기
-vis=[0 for _ in range(n+1)]
-arr=[0 for _ in range(n+1)]
-def choose(k): #현재까지 k개의 숫자를 선택
-  if(k == m):
-    for i in range(m):
-      print(str(arr[i])+" ",end="")
-    print()
-  else:
-    for i in range(n):
-      if(vis[i] == 0):
-        vis[i] = 1
-        arr[k] = i+1
-        choose(k+1)
-        vis[i] = 0
-  
-choose(0)
-  
+    else:
+        for i in arr:
+            if not vis[i]:
+                temp.append(i)
+                vis[i] = True
+                func(cnt+1)
+                temp.pop()
+                vis[i] = False
+
+
+func(0)
